9d1efd4ccae7455f945003b5ddcf01682586c942
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2022-12-14 13:50:37 +0100

    Understanding the file structure and content of files

---
+++ BulkyBookWeb/Controllers/VehicleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

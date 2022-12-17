1445ed414247403f9297f8216a442c4f357a0ce0
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2022-12-17 16:36:24 +0100

    Retrieve all Category

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
